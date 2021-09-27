d19ed0e6f5376b8a02dee69a24dedc192b9ee22e
djeko03 <88455193+djeko03@users.noreply.github.com>
2021-09-27 13:54:02 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,27 +2,27 @@ const startBtn = document.getElementsByClassName('startButton')[0];
 const answerInp = document.querySelector('#answerInput');
 const resetBtn = document.getElementsByClassName('resetButton')[0];
 
-let num;
-let evenNum = [];
+
+
+
+startBtn.addEventListener('click', getNum);
+resetBtn.addEventListener('click', reloadInterval);
 
 function reloadInterval() {
     window.location.reload();
 }
 
-startBtn.addEventListener('click', getNum);
-resetBtn.addEventListener('click', reloadInterval);
-
-
-
 function getNum() {
-    num = prompt('Введите число от 0 до 2000000: ');
-    if(num > -1 && num <= 2000000){
+    let num = prompt('Введите число от 0 до 2000000: ');
+    let evenNum = [];
+    if(num > 0 && num <= 2000000){
             for (i = 0; i <= num ; i += 2) {
                 evenNum.push(i);
                 answerInp.value = ('Чётных чисел: ' + evenNum.length);
             } 
     } else {
-        alert('Вы ввели неправильное число!');
+        alert('Надо ввести число от 0 до 2000000');
+        reloadInterval();
     }
-    
+    console.log(evenNum);
 }
